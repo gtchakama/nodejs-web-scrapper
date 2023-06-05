481917f638a0eb77cf893d6a7424a0193b7d7d43
George T Chakama <37905683+gtchakama@users.noreply.github.com>
2023-06-05 15:33:54 +0200

    fix - PORT

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get('/scrape', async (req, res) => {
 });
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
